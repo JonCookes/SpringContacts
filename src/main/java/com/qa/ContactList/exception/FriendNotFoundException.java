979d735e43c694c89d729526c9f4d03183d06019
FIRST_NAME LAST_NAME <My_NAME@example,com>
2022-02-07 11:59:43 +0000

    Test

--- src/main/java/com/qa/ContactList/exception/FriendNotFoundException.java
+++ src/main/java/com/qa/ContactList/exception/FriendNotFoundException.java
@@ -1,6 +1,6 @@
 package com.qa.ContactList.exception;
 
-public class FriendNotFoundException extends RuntimeException{
+public class FriendNotFoundException extends RuntimeException{ //sigh
 	public FriendNotFoundException(String message) {
 		super(message);
 	}
